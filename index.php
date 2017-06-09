c3277c5f9090c73606b9f8ff3b30a32c75f6adc4
HariniParth <parth.harini@gmail.com>
2017-06-09 14:21:25 -0700

    Designed landing page

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Digital Energy, Inc.</title>
+        <meta name="description" content="Digital Energy Website" />
+        <meta name="keywords" content="website, business, store" />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+        <!-- CSS
+        ================================================== -->
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+        <link type="text/css" rel="stylesheet" href="css/menusm.css" />
+        <link type="text/css" rel="stylesheet" href="css/skeleton.css" />
+        <link type="text/css" rel="stylesheet" href="css/layout.css" />
+
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/menusm.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <!-- slides START -->
+        <link type="text/css" rel="stylesheet" media="screen" href="nivo-slider/css/dark.css" />
+        <link type="text/css" rel="stylesheet" media="screen" href="nivo-slider/css/nivo-slider.css" />
+        <script type="text/javascript" src="nivo-slider/js/jquery.nivo.slider.js"></script>
+        <script type="text/javascript">
+        $(window).load(function() {
+            $('#slider').nivoSlider();
+        });
+        </script>
+        <!-- slides END -->
+        <!-- PrettyPhoto Starts -->
+        <link type="text/css" rel="stylesheet" href="prettyPhoto/css/prettyPhoto.css" />
+        <script type="text/javascript" src="prettyPhoto/js/jquery.prettyPhoto.js"></script>
+        <!-- PrettyPhoto Ends -->
+        <!-- Preloader Starts -->
+        <link type="text/css" rel="stylesheet" href="preloader/css/preloader.css" />
+        <script type="text/javascript" src="preloader/js/jquery.preloader.js" charset="utf-8"></script>
+        <!-- Preloader Ends -->
+        <!-- Ui To Top Starts -->
+        <link type="text/css" rel="stylesheet" href="ui_totop/css/ui.totop.css" />
+        <script type="text/javascript" src="ui_totop/js/jquery.ui.totop.js" charset="utf-8"></script>
+        <!-- Ui To Top Ends -->
+        <!-- DC Twitter Starts -->
+        <link type="text/css" rel="stylesheet" href="twitter_tweet/jquery.tweet.css" />
+        <script type="text/javascript" src="twitter_tweet/jquery.tweet.js" charset="utf-8"></script>
+        <!-- DC Twitter End -->
+        <!-- thumbnailScroller Starts-->
+        <link type="text/css" rel="stylesheet" href="css/jquery.thumbnailScroller.css" />
+        <script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
+        <script type="text/javascript">
+            (function($){
+                window.onload=function(){ 
+                    $("#tS2").thumbnailScroller({ 
+                        scrollerType:"clickButtons", 
+                        scrollerOrientation:"horizontal", 
+                        scrollSpeed:2, 
+                        scrollEasing:"easeOutCirc", 
+                        scrollEasingAmount:600, 
+                        acceleration:1, 
+                        noScrollCenterSpace:10, 
+                        autoScrolling:0, 
+                        autoScrollingSpeed:1000, 
+                        autoScrollingEasing:"easeInOutQuad", 
+                        autoScrollingDelay:500 
+                    });
+                };
+            })(jQuery);
+        </script>
+        <script type="text/javascript" src="js/jquery.thumbnailScroller.js"></script>
+        <!-- thumbnailScroller End-->
+        <!-- Config User Intarface Box Import START -->
+        <script type="text/javascript" src="js/head_html_default_block.js"></script>
+        <script type="text/javascript" src="js/head_html_block.js"></script>
+        <!-- Config User Intarface Box Import END -->
+        <link type="text/css" rel="stylesheet" media="screen" href="css/website.css" />
+        <script type="text/javascript" src="js/jquery.tinycarousel.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#slider1').tinycarousel();
+            });
+        </script>
+
+        <script type="text/javascript">
+            $(function (){
+                $('#menu-top-menu').tinyNav({
+                    active: 'selected',
+                    header: 'Navigation' 
+                });
+            });
+        </script>
+        <script type="text/javascript" src="js/tinynav.min.js"></script>
+    </head>
+    <body>
+        <div class="body_pattern">
+            <div class="index_page">
+                <div class="main">
+                    <div class="header">
+                        <div class="container">
+                            <div class="sixteen columns">
+                                <div class="header_resize"> 
+                                    <!-- logo -->
+                                    <div class="logo">
+                                        <div><a href="index.php"><img src="images/logo.png" alt="image"></img></a><br/>
+                                    </div>
+                                    </div>
+                                    <!-- logo --> 
+                                    <!-- menu -->
+                                    <div class="menu"> 
+                                      <script type="text/javascript" src="js/menu_html_block.js"></script> 
+                                    </div>
+                                    <!-- /menu -->
+                                    <div class="clr"></div>
+                                </div>
+                            </div>
+                        </div>
+                      <!-- now_page --> 
+                    </div>
+                    <div class="clr"></div>
+                    <div class="content">
+                        <div  id="gallery">
+                            <div class="now_page som">
+                                <div class="container">
+                                    <div class="sixteen columns">
+                                        <div class="now_page_resize">
+                                            <p>Where Energy Meets Innovation! <br/> Providing exceptional consulting services to the educational segment for over 25 years!</p>
+
+                                            <div id="now_slider">
+                                                <div class="slider_box">
+                                                    <div id="slider" class="nivoSlider">
+                                                        <img src="images/slides/slide1.jpg" /> 
+                                                        <img src="images/slides/slide2.jpg" /> 
+                                                        <img src="images/slides/slide3.jpg" /> 
+                                                        <img src="images/slides/slide4.jpg" /> 
+                                                        <img src="images/slides/slide5.jpg" /> 
+                                                    </div>
+                                                    <div class="clr"></div>
+                                                    <div class="clr"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- /now_page --> 
+                                </div>
+                                <div class="clr"></div>
+                                <div class="clr"></div>
+                                <div class="columns1">
+                                    <div class="container">
+                                        <div class="sixteen columns">
+                                            <div class="now_page_resize">
+                                                <div><h1>Our Services</h1></div><br/>
+                                                <div class="colums_box no_bg colums_box_1 first">
+                                                    <strong><img src="images/r_arrow.png" alt="" />&nbsp; Building and Plant Engineering Design and Review</strong>
+                                                </div>
+                                                <div class="colums_box no_bg colums_box_2">
+                                                    <strong><img src="images/r_arrow.png" alt="" /> Energy Infrastructure <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Planning and Assessment</strong>
+                                                </div>
+                                                <div class="colums_box no_bg colums_box_3">
+                                                    <strong><img src="images/r_arrow.png" alt="" /> Energy Efficiency <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Studies and Retrofits</strong>
+                                                </div>
+                                                <div class="colums_box no_bg colums_box_4">
+                                                    <strong><img src="images/r_arrow.png" alt="" /> Central Heating and <br/> &nbsp;&nbsp;&nbsp;&nbsp; Cooling Plant(s) Design</strong>
+                                                </div>
+                                                <div class="clr"></div>
+                                            </div>
+                                            <div class="clr"></div>
+                                        </div>
+                                    </div>
+                                    <div class="clr"></div>
+                                    <div class="container">
+                                        <div class="sixteen columns">
+                                            <div class="now_page_resize">
+                                                <div class="colums_box no_bg colums_box_1 first">
+                                                    <strong><img src="images/r_arrow.png" alt="" /> Cogeneration Plant <br/> &nbsp;&nbsp;&nbsp;&nbsp; Powering and Optimization</strong>
+                                                </div>
+                                                <div class="colums_box no_bg colums_box_2">
+                                                    <strong><img src="images/r_arrow.png" alt="" /> Building Commissioning <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; and Design Peer Reviews</strong>
+                                                </div>
+                                                <div class="colums_box no_bg colums_box_3">
+                                                    <strong><img src="images/r_arrow.png" alt="" /> Utility Metering <br/> &nbsp;&nbsp; and Energy Dashboards</strong>
+                                                </div>
+                                                <div class="colums_box no_bg colums_box_4">
+                                                    <strong><img src="images/r_arrow.png" alt="" /> Engineering Systems <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Design and Review</strong>
+                                                </div>
+                                                <div class="clr"></div>
+                                            </div>
+                                            <div class="clr"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <div class="clr"></div>
+                        </div>
+                        <div class="clr"></div>
+                        <!-- /content --> 
+                    </div>
+                    <div class="clr"></div>
+
+                <!-- prefooter -->
+                    <div class="prefooter">
+                        <div class="prefooter_bg">
+                            <div class="container">
+                                <div class="sixteen columns">
+                                    <div class="prefooter_resize">
+                                        <div class="fcol">
+                                          <h3>Latest News</h3>
+                                            <!-- DC Twitter Start -->
+                                            <!-- Generate your own embedded timeline: https://dev.twitter.com/docs/embedded-timelines -->
+                                            <a class="twitter-timeline" href="https://twitter.com/DigitalEnergyHQ" data-widget-id="398592013018226688" width="230" height="200" data-chrome="nofooter noheader transparent noscrollbar" data-theme="dark" data-link-color="#ccc">Tweets</a>
+                                            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                                          <!-- DC Twitter End -->
+                                        </div>
+                                        <div class="fcol">
+                                          <h3>Stay Connected</h3>
+                                            <p>
+                                                <strong>Digital Energy, Inc.</strong> <br /><br />
+                                                128 Auburn Ct Ste 106 <br />
+                                                Westlake Village, CA <br />
+                                                91362 <br />
+                                                USA <br /><br />
+                                                Phone: (805) 374-1777 <br />
+                                                Contact: info@digitalenergy.com <br />
+                                            </p>
+                                        </div>
+                                        <div class="fcol">
+                                            <h3>Our Commitment</h3>
+                                            <p>
+                                                We foster creative minds for out of the box thinking while building innovative products and services that motivate us to keep moving forward. <br /><br />
+                                                Where Energy Meets Innovation! That's our Motto. We pour our hearts and souls into providing products and services to our clients that make them our success story.
+                                            </p>
+                                        </div>
+                                        <div class="fcol last">
+                                            <h3>Keep Updated</h3>
+                                            <p>Subscribe to our mailing list for offers, news updates and more!</p>
+
+                                            <!-- Mailing List Form -->
+                                            <div id="mailform-subscribe">
+                                                <form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                                                    <input type="email" size="15" value="Your Email" name="EMAIL" class="required email" id="mce-EMAIL" onfocus="if(this.value===this.defaultValue)this.value='';" onblur="if(this.value==='')this.value=this.defaultValue;" />
+                                                    <div id="mce-responses" class="clear">
+                                                        <div class="response" id="mce-error-response" style="display:none"></div>
+                                                        <div class="response" id="mce-success-response" style="display:none"></div>
+                                                    </div>
+                                                    <div>
+                                                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <!-- Mailing List Form End -->
+                                        </div>
+                                        <div class="clr"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="clr"></div>
+                            <div class="prefooter_resize_bottom"></div>
+                            <div class="clr"></div>
+                        </div>
+                        <div class="clr"></div>
+                    </div>
+                    <!-- /prefooter -->
+                    <div class="clr"></div>
+
+                    <!-- footer -->
+                    <div class="footer">
+                        <div class="container">
+                            <div class="sixteen columns">
+                                <div class="footer_resize">
+                                    <p>Content &copy; 2017 Digital Energy, Inc. All Rights Reserved.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clr"></div>
+                    </div>
+                    <!-- footer -->
+                    <div class="clr"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
